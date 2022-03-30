59471889b9ecf561f66e42c21e89563790fee70f
Dennis Hallerbach <dennis.hallerbach@hs-augsburg.de>
2022-03-30 13:24:20 +0200

    added location support and new csv file

--- Main.ps1
+++ Main.ps1
@@ -1,38 +1,52 @@
 ﻿
 # Create a user with the according rights before and exchange witht the correct tenant
 Connect-AzAccount -Tenant '00000000-0000-0000-0000-000000000000' 
-$tenantId = (Get-AzContext).Tenant.Id
-
+# Variable for defining the location, which is used to filter 
+# example "southeastasia"
+$location = "southeastasia"
+#Generate csv File to later convertation to Excel file. Fill in your path!
+# example ".\output.xlsx"
+$outfile = "filepath_for_temporary_csv_file"
+$outputXLSX = "filepath_for_Excel_report"
+#safe headers for csv file
+Add-Content -Path $outfile  -Value '"Subscription_Name","Subscription_ID","Role_Name","Username","Email"'
 $allResources = @()
-$subscriptions=Get-AzureRMSubscription
+$subscriptions=Get-AzSubscription
 
 ForEach ($vsub in $subscriptions){
-    Select-AzureRmSubscription $vsub.SubscriptionID
+    #Select curent subscription to get ressource details
+    Select-AZSubscription $vsub.SubscriptionID
 
     Write-Host
     Write-Host “Working on “ $vsub
     Write-Host
-
-    $allResources += $allResources |Select-Object $vsub.SubscriptionID,$vsub.Name
-
-    Get-AzureRmVM | Select Name,REsourceGroupName,Location
-
-    # Fetch list of all directory roles with template ID
-    Get-AzureADMSRoleDefinition
-
-    # Fetch a specific directory role by ID
-    $role = Get-AzureADMSRoleDefinition -Id $vsub
-
-    # Fetch membership for a role
-    Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '$($role.Id)'" | Export-Csv az_user_role.csv
-
-
+    # Fetch all Ressources in Subscription
+    $allResources = Get-AzResource
+    ForEach ($resource in $allResources){
+        # Check if Ressourcelocation is the same as $location variable
+        if($resource.Location -eq $location){
+            # Get Role-Assignements of subscription
+             $Roleassignment = Get-AzRoleAssignment
+             # Import csv file for appending a new row
+             $csvimport = Import-Csv $outfile
+             # Create new Custom Object to append to CSV
+             $newrow = [PSCustomObject] @{
+                "Subscription_Name" = $vsub.Name;
+                "Subscription_ID" = $vsub.SubscriptionId;
+                "Role_Name" = $Roleassignment.RoleDefinitionName;
+                "Username" = $Roleassignment.DisplayName;
+                "Email" = $Roleassignment.SignInName;
+            }
+            $newrow | Export-CSV $outfile -Append -NoTypeInformation
+        }
+    }
+}
+        
     ############### Convert to excel #####################
 
 
     ### Set input and output path
-    $inputCSV = "C:\ps\az_user_role.csv"
-    $outputXLSX = "C:\ps\az_user_role.xlsx"
+    $inputCSV = $outfile
 
     ### Create a new Excel Workbook with one empty sheet
     $excel = New-Object -ComObject excel.application 
@@ -41,12 +55,12 @@ ForEach ($vsub in $subscriptions){
 
     ### Build the QueryTables.Add command
     ### QueryTables does the same as when clicking "Data » From Text" in Excel
-    $TxtConnector = ("TEXT;" + $inputCSV)
+    $TxtConnector = ("TEXT;" + $outfile)
     $Connector = $worksheet.QueryTables.add($TxtConnector,$worksheet.Range("A1"))
     $query = $worksheet.QueryTables.item($Connector.name)
 
-    ### Set the delimiter (, or ;) according to your regional settings
-    $query.TextFileOtherDelimiter = $Excel.Application.International(5)
+    ### Set the delimiter to ","
+    $query.TextFileOtherDelimiter = ","
 
     ### Set the format to delimited and text for every column
     ### A trick to create an array of 2s is used with the preceding comma
@@ -61,6 +75,7 @@ ForEach ($vsub in $subscriptions){
     ### Save & close the Workbook as XLSX. Change the output extension for Excel 2003
     $Workbook.SaveAs($outputXLSX,51)
     $excel.Quit()
+    # remove csv File
+    Remove-Item $outfile
 
-}
 
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AzureUserRoleExport
-Sample Script to Export all Users of each Subscription within a tenant (according to login-user to Excel
+Sample Script to Export all Users of each Subscription within a tenant (according to login-user and location of ressources)  to Excel
 
 # PLEASE READ # 
 
